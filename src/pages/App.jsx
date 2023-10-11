38691f08fa6fb13583c089a0fd1f3e12e2e66204
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-10-12 01:59:57 +0900

    Update add bucket

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SizeLayout from "./router/sizeLayout";
 import Main from "./Main";
+import Bucket from "./Bucket";
 
 function App() {
   return (
     <Routes>
-      <Route element={<SizeLayout/>}>
-        <Route path="/" element={<Main/>}/>
+      <Route element={<SizeLayout />}>
+        <Route path="/" element={<Main />} />
+        <Route path="/bucket" element={<Bucket />} />
       </Route>
     </Routes>
   );
