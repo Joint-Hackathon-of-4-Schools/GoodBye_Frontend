e474488d8e189a67659e753c6cfc9cf31e3d3a0c
eternrust <overload829@gmail.com>
2023-10-12 18:04:57 +0900

    add image

--- src/components/menuType.jsx
+++ src/components/menuType.jsx
@@ -4,9 +4,9 @@ import styled from "styled-components";
 const MenuType = ({children, imageURL, ...args}) => {
   return (
     <Container
-      img={imageURL}
       {...args}
     >
+      <Img src={imageURL} />
       <TextBox>
         {children}
       </TextBox>
@@ -20,13 +20,18 @@ const Container = styled.div`
   width: 200px;
   height: 200px;
   background: #F5F5F5;
-  background-image: url(${({img}) => img});
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center center;
   border-radius: 20px;
   display: flex;
-  align-items: flex-end;
+  flex-direction: column;
+`
+
+const Img = styled.img.attrs({
+  alt: '음료 이미지'
+})`
+  width: 200px;
+  height: calc(100% - 80px);
+  object-fit: contain;
+  object-position: center center;
 `
 
 const TextBox = styled.span`
@@ -38,7 +43,7 @@ const TextBox = styled.span`
   font-family: Inter;
   font-style: normal;
   font-size: 32px;
-  font-weight: 400;
+  font-weight: 600;
   text-align: center;
   white-space: pre-line;
 `
