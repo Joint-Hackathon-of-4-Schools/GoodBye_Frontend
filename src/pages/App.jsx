bf8f0c4f5c7433e43f196058737bdbdd1e95a238
eternrust <eternrust@dsm.hs.kr>
2023-10-12 14:37:41 +0900

    update add promotion

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -6,12 +6,14 @@ import Bucket from "./Bucket";
 import Recommend from "./Recommend";
 import Combine from "./Combine";
 import AIRecommned from "./AIRecommend";
+import Promotion from "./promotion";
 
 function App() {
   return (
     <Routes>
       <Route element={<SizeLayout />}>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Promotion />} />
+        <Route path="/main" element={<Main />} />
         <Route path="/bucket" element={<Bucket />} />
         <Route path="/recommend" element={<Recommend />} />
         <Route path="/recommend/combine" element={<Combine />} />
